ae2270b7083b0dcccd1b913e5f4eb786b010474e
kei95 <kei0613shine.star@gmail.com>
2022-07-23 17:32:23 -0700

    add title text and update app/index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,70 +2,12 @@ import Header from "~/components/Header";
 
 export default function Index() {
   return (
+    <>
       <Header />
       <div className="pt-4 px-8">
-
-          <div className="bg-white rounded-md px-4 py-4 border-2 border-black">
-            <h2 className="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 className="text-lg text-zinc-500 mb-3">Tailwind CSS</h1>
-            <p className="leading-relaxed text-sm mb-3">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-            <button className="bg-stone-600	text-sm text-white rounded-md px-4 py-1">
-              Click Me
-            </button>
-          </div>
-
-          <div className="bg-white rounded-md px-4 py-4 border-2 border-black">
-            <h2 className="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 className="text-lg text-zinc-500 mb-3">Tailwind CSS</h1>
-            <p className="leading-relaxed text-sm mb-3">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-            <button className="bg-stone-600	text-sm text-white rounded-md px-4 py-1">
-              Click Me
-            </button>
-          </div>
-
-          <div className="bg-white rounded-md px-4 py-4 border-2 border-black">
-            <h2 className="tracking-widest text-xs mb-1">CATEGORY</h2>
-            <h1 className="text-lg text-zinc-500 mb-3">Tailwind CSS</h1>
-            <p className="leading-relaxed text-sm mb-3">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-            <button className="bg-stone-600	text-sm text-white rounded-md px-4 py-1">
-              Click Me
-            </button>
-          </div>
-        </div>
+        <h1 className="title-text">Hi. It's Kei.</h1>
+        <h1 className="title-text">A developer.</h1>
       </div>
-    </div>
+    </>
   );
 }
