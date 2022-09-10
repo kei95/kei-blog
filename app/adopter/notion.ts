dc25af7728909d7fa319981a4ed730bd4c361b1b
kei95 <kei0613shine.star@gmail.com>
2022-09-09 20:17:41 -0700

    move notion-specific functions to adopter from seed and add coverUrl

--- app/adopter/notion.ts
+++ app/adopter/notion.ts
@@ -1,6 +1,78 @@
 import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "notion-to-md";
+import type { Post } from "@prisma/client";
+import type {
+  PageObjectResponse,
+  PropertyItemPropertyItemListResponse,
+  TitlePropertyItemObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
 
 export const notion = new Client({
   auth: process.env.NOTION_SECRET,
   notionVersion: "2022-06-28",
 });
+
+/** Extract cover image's url from given page */
+function getCoverImageUrlFromPage(page: PageObjectResponse): string {
+  const coverObj = page.cover;
+  if (coverObj?.type === "external") {
+    return coverObj.external.url;
+  }
+  return "";
+}
+
+/** Gets page content as markdown */
+export async function getPostMarkdownById(id: string): Promise<{
+  markdown: string;
+}> {
+  const notionToMarkdown = new NotionToMarkdown({ notionClient: notion });
+
+  const notionPage = await notionToMarkdown.pageToMarkdown(id || "");
+  const markdown = notionToMarkdown.toMarkdownString(notionPage);
+
+  return { markdown };
+}
+
+export async function getNotionPages(): Promise<PageObjectResponse[]> {
+  const database = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID || "",
+    sorts: [{ direction: "ascending", timestamp: "created_time" }],
+  });
+
+  if (!database) throw new Error("Failed to retrieve database");
+
+  const pages = database.results as PageObjectResponse[];
+
+  return pages;
+}
+
+/** Converts given page to Post object to insert/upsert to db */
+export async function getPostObjectFromPage(
+  page: PageObjectResponse
+): Promise<Post> {
+  const titleId = page.properties.Name.id;
+  const pageProperties = (await notion.pages.properties.retrieve({
+    page_id: page.id,
+    property_id: titleId,
+  })) as PropertyItemPropertyItemListResponse;
+
+  // extract title of the post
+  const [titleProperty] = pageProperties.results as [
+    TitlePropertyItemObjectResponse
+  ];
+
+  // Post contents
+  const pageTitle = titleProperty.title.plain_text;
+  const coverImageUrl = getCoverImageUrlFromPage(page);
+  const { markdown } = await getPostMarkdownById(page.id);
+
+  const post: Post = {
+    id: page.id,
+    title: pageTitle,
+    coverUrl: coverImageUrl,
+    markdown,
+    createdAt: new Date(page.created_time),
+  };
+
+  return post;
+}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,61 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { NotionToMarkdown } from "notion-to-md";
 
-import { notion } from "../app/adopter/notion";
-import type {
-  PageObjectResponse,
-  PropertyItemPropertyItemListResponse,
-  TitlePropertyItemObjectResponse,
-} from "@notionhq/client/build/src/api-endpoints";
+import { getNotionPages, getPostObjectFromPage } from "../app/adopter/notion";
 
 const prisma = new PrismaClient();
 
-/** Gets page content as markdown */
-export async function getPostMarkdownById(id: string): Promise<{
-  markdown: string;
-}> {
-  const notionToMarkdown = new NotionToMarkdown({ notionClient: notion });
-
-  const notionPage = await notionToMarkdown.pageToMarkdown(id || "");
-  const markdown = notionToMarkdown.toMarkdownString(notionPage);
-
-  return { markdown };
-}
-
 const upsertBlogPosts = async () => {
-  // ======== first part - get database ========
-  const database = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID || "",
-    sorts: [{ direction: "ascending", timestamp: "created_time" }],
-  });
-
-  if (!database) throw new Error("Failed to retrieve database");
-
-  const pages = database.results as PageObjectResponse[];
-
-  // ======== second part - get page titles ========
+  const pages = await getNotionPages();
 
   // loop through each page of retrieved database to upsert each items
   for (const page of pages) {
-    const titleId = page.properties.Name.id;
-    const pageProperties = (await notion.pages.properties.retrieve({
-      page_id: page.id,
-      property_id: titleId,
-    })) as PropertyItemPropertyItemListResponse;
-
-    // extract title of the post
-    const [titleProperty] = pageProperties.results as [
-      TitlePropertyItemObjectResponse
-    ];
-    const pageTitle = titleProperty.title.plain_text;
-
-    // extract page's content as markdown
-    const { markdown } = await getPostMarkdownById(page.id);
-
-    const post = Object.assign(
-      {},
-      { id: page.id, title: pageTitle, markdown, createdAt: page.created_time }
-    );
+    const post = await getPostObjectFromPage(page);
 
     await prisma.post.upsert({
       where: { id: page.id },
