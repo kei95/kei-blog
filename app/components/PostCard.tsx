6f01b0593d5db875f8b6ca2ac2a4ae4d7003b60e
kei95 <kei0613shine.star@gmail.com>
2022-09-11 17:29:14 -0700

    Add PostCard component

---
+++ app/components/PostCard.tsx
@@ -0,0 +1,34 @@
+import type { Post } from "@prisma/client";
+import { getPostCreatedAt } from "~/lib/date";
+
+import RouteLink from "~/lib/RouteLink";
+
+interface PostCardProps {
+  post: Post;
+}
+
+export default function PostCard({ post }: PostCardProps) {
+  return (
+    <RouteLink
+      to={`posts/${post.id}`}
+      className="col-span-4 bg-background_darken rounded-md"
+    >
+      {post.coverUrl ? (
+        <img
+          className="h-80 w-full rounded-md"
+          src={post.coverUrl ?? ""}
+          alt={`Cover`}
+        />
+      ) : (
+        <div className="h-80 w-full flex justify-center align-middle items-center rounded-md bg-white">
+          <h1 className="title-text-h1 text-4xl text-center">Blog post</h1>
+        </div>
+      )}
+
+      <div className="px-4 py-4">
+        <h3 className=" text-text pb-4">{getPostCreatedAt(post.createdAt)}</h3>
+        <h2>{post.title}</h2>
+      </div>
+    </RouteLink>
+  );
+}
