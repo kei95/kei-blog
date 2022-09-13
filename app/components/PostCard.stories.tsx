a07c89c80c1b8778738a99b57e9a40da9948b616
kei95 <kei0613shine.star@gmail.com>
2022-09-13 00:06:37 -0700

    Add PostCard to storybook

---
+++ app/components/PostCard.stories.tsx
@@ -0,0 +1,29 @@
+import type { Post } from "@prisma/client";
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import PostCard from "./PostCard";
+
+export default {
+  title: "PostCard",
+  component: PostCard,
+} as ComponentMeta<typeof PostCard>;
+
+const mockedPost: Post = {
+  id: "neque-porro-quisquam-est",
+  title: "Lorem_Ipsum",
+  coverUrl: "https://www.notion.so/images/page-cover/woodcuts_15.jpg",
+  createdAt: new Date("2022-08-29T03:22:00.000Z"),
+  markdown: "",
+};
+
+const Template: ComponentStory<typeof PostCard> = () => (
+  <PostCard post={mockedPost} />
+);
+
+export const PostWithCover = Template.bind({});
+
+export const PostWithoutCover = Template.bind({});
+
+PostWithoutCover.args = {
+  post: Object.assign({}, mockedPost, { ...mockedPost, coverUrl: null }),
+};
