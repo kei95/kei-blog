657ce57fed9370838deb73609eb7b80f0cc1e8ee
kei95 <kei0613shine.star@gmail.com>
2022-09-14 22:29:31 -0700

    Tweak JobDescription component's style

--- app/components/JobDescription.tsx
+++ app/components/JobDescription.tsx
@@ -21,48 +21,44 @@ export default function JobDescription({
   const { title, period, description, roles, tools, isCurrentJob } = job;
 
   return (
-    <div className="max-w-screen-sm" data-testid={JOB_DESCRIPTION_TEST_ID}>
-      <div className="flex flex-row">
-        {/* Left side */}
-        <div className="flex flex-col items-start">
-          <RadioWaveButton
-            animationState={isCurrentJob ? "ACTIVE" : "INACTIVE"}
-            animationSize={20}
-            buttonSize={8}
-            className="-mt-1.5 -ml-6"
-          />
+    <div className="flex flex-row" data-testid={JOB_DESCRIPTION_TEST_ID}>
+      {/* Left side */}
+      <div className="flex flex-col items-start">
+        <RadioWaveButton
+          animationState={isCurrentJob ? "ACTIVE" : "INACTIVE"}
+          animationSize={20}
+          buttonSize={8}
+          className="-mt-1.5 -ml-6"
+        />
 
-          {/* vertical line */}
-          <div className=" border-l-2 mt-1 ml-2.5 border-l-white h-full" />
+        {/* vertical line */}
+        <div className=" border-l-2 mt-1 ml-2.5 border-l-white h-full" />
+      </div>
+
+      {/* Right side */}
+      <div className="flex-wrap overflow-hidden">
+        <div className="flex flex-row ">
+          <h3>{title}</h3>
+          <p className=" pl-4 text-textAccent">{period}</p>
         </div>
 
-        {/* Right side */}
-        <div className="flex flex-col">
-          <div className="flex items-end">
-            <h3 className="pr-4">{title}</h3>
-            <p>{period}</p>
-          </div>
+        <p className="pb-3">{description}</p>
 
-          <p className="pb-3">{description}</p>
+        {/* roles list */}
+        <p className="font-bold text-textAccent">Key roles</p>
+        <ul className="list-disc list-inside pb-3">
+          {roles.map((role, idx) => (
+            <li key={`${idx}_${role}`}>{role}</li>
+          ))}
+        </ul>
 
-          {/* roles list */}
-          <p className="font-bold text-textAccent">Key roles</p>
-          <ul className="list-disc list-inside pb-3">
-            {roles.map((role, idx) => (
-              <li key={`${idx}_${role}`}>{role}</li>
-            ))}
-          </ul>
-
-          {/* tools list */}
-          <span className="font-bold text-textAccent">
-            Tools:{" "}
-            {tools.map((tool, idx) => (
-              <span key={tool} className="pr-1 font-normal">
-                {idx < tools.length - 1 ? `${tool},` : tool}
-              </span>
-            ))}
+        {/* tools list */}
+        <span className="font-bold text-textAccent">Tools: </span>
+        {tools.map((tool, idx) => (
+          <span key={tool} className="pr-1 font-normal">
+            {idx < tools.length - 1 ? `${tool},` : tool}
           </span>
-        </div>
+        ))}
       </div>
     </div>
   );
